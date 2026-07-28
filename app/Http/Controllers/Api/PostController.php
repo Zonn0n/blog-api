055c09d2b3872436d411ef09414c8b0a0dfafac0
Zonn0n <maksimka.leukhin@mail.ru>
2026-07-28 13:34:47 +0300

    PostController refactor

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,83 +7,37 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PostController extends Controller
 {
+    private const DEFAULT_LIMIT = 20;
+    private const MAX_LIMIT = 100;
+
+    private const DEFAULT_OFFSET = 0;
+
     public function index(Request $request) 
     {
-        $limit = $request->integer('limit', 20);
-        $offset = $request->integer('offset', 0);
-        $sort = SortingType::tryFrom(
-            $request->query('sort', SortingType::default()->value)
-        ) ?? SortingType::default();
-
-        $query = Post::query();
-        if ($request->filled('date_from')) {
-            $query->whereDate(
-                'created_at',
-                '>=',
-                $request->date('date_from'),
-            );
-        }
-        if ($request->filled('date_to')) {
-            $query->whereDate(
-                'created_at',
-                '<=',
-                $request->date('date_to'),
-            );
-        }
-
-        return PostResource::collection(
-            $query
-                ->orderBy($sort->value)
-                ->offset($offset)
-                ->limit($limit)
-                ->get()
+        return $this->getPostsCollection(
+            Post::query(),
+            $request,
         );
     }
 
     public function myPosts(Request $request) 
     {
-        $limit = $request->integer('limit', 20);
-        $offset = $request->integer('offset', 0);
-        $sort = SortingType::tryFrom(
-            $request->query('sort', SortingType::default()->value)
-        ) ?? SortingType::default();
-
-        $query = $request
-            ->user()
-            ->posts();
-        if ($request->filled('date_from')) {
-            $query->whereDate(
-                'created_at',
-                '>=',
-                $request->date('date_from'),
-            );
-        }
-        if ($request->filled('date_to')) {
-            $query->whereDate(
-                'created_at',
-                '<=',
-                $request->date('date_to'),
-            );
-        }
-
-        return PostResource::collection(
-            $query
-                ->orderBy($sort->value)
-                ->offset($offset)
-                ->limit($limit)
-                ->get()
+        return $this->getPostsCollection(
+            $request->user()->posts(),
+            $request,
         );
     }
 
     public function show(int $id) 
     {
-        return new PostResource(
-            Post::findOrFail($id)
-        );
+        return new PostResource(Post::findOrFail($id));
     }
 
     public function store(StorePostRequest $request)
@@ -92,8 +46,45 @@ class PostController extends Controller
             $request->validated()
         );
 
-        return new PostResource(
-            $post
+        return new PostResource($post);
+    }
+
+    private function getPostsCollection(
+        Builder|HasMany $query, 
+        Request $request
+    ): AnonymousResourceCollection
+    {
+        $limit = min(
+            $request->integer('limit', self::DEFAULT_LIMIT), 
+            self::MAX_LIMIT
+        );
+        $offset = $request->integer('offset', self::DEFAULT_OFFSET);
+        $sort = SortingType::tryFrom(
+            $request->query('sort', SortingType::default()->value)
+        ) ?? SortingType::default();
+
+        if ($request->filled('date_from')) {
+            $query->whereDate(
+                'created_at',
+                '>=',
+                $request->date('date_from'),
+            );
+        }
+
+        if ($request->filled('date_to')) {
+            $query->whereDate(
+                'created_at',
+                '<=',
+                $request->date('date_to'),
+            );
+        }
+
+        return PostResource::collection(
+            $query
+                ->orderBy($sort->value)
+                ->offset($offset)
+                ->limit($limit)
+                ->get()
         );
     }
 }
